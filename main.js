c6cedbbadb2011be5baf74ac3b98694d49de6811
Stephen Mann <thanthese@gmail.com>
2014-12-04 21:23:28 -0600

    Ugly CSS speed test hack.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // To start skewer mode: `httpd-start` and open page in browser.
 
-// coordinates: (row, col), with (0, 0) in the upper left
+// coordinates: (x, y), lower left is (0, 0)
 
 var settings = {
     board: {
@@ -21,13 +21,13 @@ function setup() {
     $("#game").html("");
 
     // build grid
-    for (var y = 0; y < settings.board.height; y++) {
+    for (var y = settings.board.height - 1; y >= 0; y--) {
         var row = $(".templates .row").clone().appendTo("#game");
         for (var x = 0; x < settings.board.width; x++) {
             $(".templates .cell")
                 .clone()
                 .text(_.sample("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.;"))
-                .attr("id", "cell-" + y + "-" + x)
+                .attr("id", getPixelId(x, y))
                 .appendTo(row);
         }
     }
@@ -37,38 +37,66 @@ function setup() {
     for (var i = 0; i < 80; ++i) {
         var x = _.random(0, settings.board.width);
         var y = _.random(0, settings.board.height);
-        $("#cell-" + x + "-" + y).text(_.sample("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.;"));
+        getPixel(x, y).text(_.sample("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.;"));
     }
 
     // add player
-    $("#cell-" + board.player.x + "-" + board.player.y).text("@");
+    getPixel(board.player.x, board.player.y).text("@");
 
     // listen for keyboard
     $(document).keypress(onkey);
+
+    $(".cell").addClass("blueBackground");
 }
 
 function onkey(event) {
-    switch (event.which) {
-        case "j".charCodeAt():
-            movePlayer(1, 0);
-            break;
-        case "k".charCodeAt():
-            movePlayer(-1, 0);
-            break;
-        case "h".charCodeAt():
-            movePlayer(0, -1);
-            break;
-        case "l".charCodeAt():
-            movePlayer(0, 1);
-            break;
+    function k(ch) {
+        return event.which == ch.charCodeAt();
     }
+
+    if (k("j")) return movePlayer(0, -1);
+    if (k("k")) return movePlayer(0, 1);
+    if (k("h")) return movePlayer(-1, 0);
+    if (k("l")) return movePlayer(1, 0);
+
+    if (k("y")) return movePlayer(-1, 1);
+    if (k("u")) return movePlayer(1, 1);
+    if (k("b")) return movePlayer(-1, -1);
+    if (k("n")) return movePlayer(1, -1);
 }
 
-function movePlayer(deltaRow, deltaCol) {
-    $("#cell-" + board.player.y + "-" + board.player.x).text(".");
-    board.player.y += deltaRow;
-    board.player.x += deltaCol;
-    $("#cell-" + board.player.y + "-" + board.player.x).text("@");
+function movePlayer(deltaX, deltaY) {
+    getPixel(board.player.x, board.player.y).text(".");
+    board.player.x += deltaX;
+    board.player.y += deltaY;
+    getPixel(board.player.x, board.player.y).text("@");
+    randomlyChangeBackground();
+}
+
+var last = true;
+function randomlyChangeBackground() {
+
+    var background = "";
+    if(last == true) {
+        last = false;
+        background="blueBackground";
+    } else {
+        last = true;
+        background="redBackground";
+    }
+
+
+    $(".cell").toggleClass("blueBackground");
+    $(".cell").toggleClass("redBackground");
+
+}
+
+function getPixelId(x, y) {
+    return "pixel-" + x + "-" + y;
+}
+
+function getPixel(x, y) {
+    return $("#" + getPixelId(x, y));
 }
 
 $(setup);
